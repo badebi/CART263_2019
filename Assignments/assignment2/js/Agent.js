3e4ab9fa09be118a6ade718688752158f2d70ab6
Ebby <ebi.badavi@yahoo.com>
2019-01-22 11:49:57 -0500

    Init

---
+++ Assignments/assignment2/js/Agent.js
@@ -0,0 +1,35 @@
+class Agent {
+  constructor(x,y,size,agentColor) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = agentColor;
+    this.active = true;
+  }
+
+  display() {
+     if (!this.active) {
+       return;
+     }
+       push();
+       noStroke();
+       fill(this.color);
+       ellipse(this.x, this.y, this.size);
+       pop();
+  }
+
+  isCollidingWith(otherAgent) {
+    if (!this.active) {
+      return;
+    }
+
+    let dis = dist(this.x, this.y, otherAgent.x, otherAgent.y);
+
+    if (dis < this.size/2 + otherAgent.size/2 ){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+}
---
+++ Assignments/assignment2/js/Avatar.js
@@ -0,0 +1,29 @@
+class Avatar extends Agent {
+  constructor(x,y,size,sizeLoss) {
+    super(x,y,size, '#ccf548');
+    this.maxSize = size;
+    this.sizeLoss = sizeLoss;
+  }
+
+  update(){
+    if (!this.active) {
+      return;
+    }
+    this.x = mouseX;
+    this.y = mouseY;
+    this.size = constrain(this.size - this.sizeLoss, 0, this.maxSize);
+    if (this.size === 0){
+      this.active = false;
+    }
+  }
+
+  eating(theFood) {
+    if (!this.active) {
+      return;
+    }
+
+    this.size = constrain(this.size + theFood.size, 0, this.maxSize);
+
+    theFood.reset();
+  }
+}
