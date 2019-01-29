439baace51180e622f6d9d418b960206232e12f7
Ebby <ebi.badavi@yahoo.com>
2019-01-29 10:50:09 -0500

    eating fly

--- ClassActivity/Week4/Eat_Up/js/script.js
+++ ClassActivity/Week4/Eat_Up/js/script.js
@@ -7,3 +7,36 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let $mouth;
+let $fly;
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $mouth = $('#mouth');
+    $fly = $('#fly');
+
+    $fly.draggable();
+    $mouth.droppable({
+      drop: flyDropped
+    });
+});
+
+
+function flyDropped (event, ui){
+  console.log("Dropped!");
+
+  // $('#fly').remove();
+  // $fly.remove();
+  ui.draggable.remove();
+
+  setInterval(chew,300);
+}
+
+function chew (){
+  if ($mouth.attr('src') === "assets/images/mouth-open.png"){
+    $mouth.attr('src', 'assets/images/mouth-closed.png');
+  } else {
+      $mouth.attr('src', 'assets/images/mouth-open.png');
+  }
+}
