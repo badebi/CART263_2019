9937de5f8624f6c15655c037380ac66108379ad8
Ebby <ebi.badavi@yahoo.com>
2019-01-29 22:49:32 -0500

    requirements achieved

--- Assignments/assignment3/js/script.js
+++ Assignments/assignment3/js/script.js
@@ -14,69 +14,85 @@ secrets become revealed!
 // A place to store the jQuery selection of all spans
 let $spans;
 
-// When the document is loaded we call the setup function
-$(document).ready(setup);
+// Secrets Found
+let secretsFound = 0;
 
-// setup()
+// // When the document is loaded we call the setup function
+// $(document).ready(setup);
 //
-// Sets the click handler and starts the time loop
-function setup() {
-  // Save the selection of all spans (since we do stuff to them multiple times)
-  $spans = $('span');
-  // Set a click handler on the spans (so we know when they're clicked)
-  $spans.on('click',spanClicked);
-  // Set an interval of 500 milliseconds to update the state of the page
-  setInterval(update,500);
-};
-
-// spanClicked()
+// // setup()
+// //
+// // Sets the click handler and starts the time loop
+// function setup() {
+//   // Save the selection of all spans (since we do stuff to them multiple times)
+//   $spans = $('span');
+//   // Set a click handler on the spans (so we know when they're clicked)
+//   $spans.on('click',spanClicked);
+//   // Set an interval of 500 milliseconds to update the state of the page
+//   setInterval(update,500);
+// };
 //
-// When a span is clicked we remove its revealed class and add the redacted class
-// thus blacking it out
-function spanClicked() {
-  $(this).removeClass('revealed');
-  $(this).addClass('redacted');
-}
-
-// update()
+// // spanClicked()
+// //
+// // When a span is clicked we remove its revealed class and add the redacted class
+// // thus blacking it out
+// function spanClicked() {
+//   $(this).removeClass('revealed');
+//   $(this).addClass('redacted');
+// }
 //
-// Update is called every 500 milliseconds and it updates all the spans on the page
-// using jQuery's each() function which calls the specified function on _each_ of the
-// elements in the selection
-function update() {
-  $spans.each(updateSpan);
-}
-
-// updateSpan()
+// // update()
+// //
+// // Update is called every 500 milliseconds and it updates all the spans on the page
+// // using jQuery's each() function which calls the specified function on _each_ of the
+// // elements in the selection
+// function update() {
+//   $spans.each(updateSpan);
+// }
 //
-// With a probability of 10% it unblanks the current span by removing the
-// redacted class and adding the revealed class. Because this function is called
-// by each(), "this" refers to the current element that each has selected.
-function updateSpan() {
-  let r = Math.random();
-  if (r < 0.1) {
-    $(this).removeClass('redacted');
-    $(this).addClass('revealed');
-  }
-}
+// // updateSpan()
+// //
+// // With a probability of 10% it unblanks the current span by removing the
+// // redacted class and adding the revealed class. Because this function is called
+// // by each(), "this" refers to the current element that each has selected.
+// function updateSpan() {
+//   let r = Math.random();
+//   if (r < 0.1) {
+//     $(this).removeClass('redacted');
+//     $(this).addClass('revealed');
+//   }
+// }
 
 // A version using anonymous functions:
 
-// $(document).ready(function () {
-//   $spans = $('span');
-//
-//   $spans.on('click',function () {
-//     $(this).removeClass('revealed');
-//     $(this).addClass('redacted');
-//   });
-//
-//   setInterval(function () {
-//     $spans.each(function () {
-//       let r = Math.random();
-//       if (r < 0.1) {
-//         $(this).removeClass('redacted');
-//         $(this).addClass('revealed');
-//       }
-//     });
-//   },500);
-// });
+$(document).ready(function () {
+  $spans = $('span:not(.secret)');
+
+
+  $spans.on('click',function () {
+    $(this).removeClass('revealed');
+    $(this).addClass('redacted');
+  });
+
+  $('.secret').on('mouseover', function(){
+    $(this).removeClass('secret');
+    $(this).addClass('found');
+
+    $(this).off();
+    secretsFound ++;
+
+        $('#secret-count').text(secretsFound);
+    console.log("secrets Found:" + secretsFound);
+
+  });
+
+  setInterval(function () {
+    $spans.each(function () {
+      let r = Math.random();
+      if (r < 0.1) {
+        $(this).removeClass('redacted');
+        $(this).addClass('revealed');
+      }
+    });
+  },500);
+});
