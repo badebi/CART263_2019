15278cc0dbb0b7c3d3c5bc642e7dd94b6125faad
Ebby <ebi.badavi@yahoo.com>
2019-02-05 04:51:07 -0500

    basic stuff
    + images added
    + bin is droppable

--- Projects/project1/js/script.js
+++ Projects/project1/js/script.js
@@ -5,9 +5,18 @@
 Project1 | Sisyphusesque
 Ebrahim (Ebby) Badawi
 
-
 ******************/
 
+let $bin;
+let $trash;
+
 $(document).ready(function () {
-  // This code will run when the document is ready
+  //  Get the bin element from the page
+  $bin = $('#bin');
+  //  make it droppable
+  $bin.droppable({
+    //  when drop is compeleted, call trashDropped function
+    drop: trashDropped
+  });
+
 });
