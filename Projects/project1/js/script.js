95bda3ca98dfb066ae41a9081a42bbcedc258c28
Ebby <ebi.badavi@yahoo.com>
2019-02-05 03:31:21 -0500

    Init

---
+++ Projects/project1/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*****************
+
+Project1 | Sisyphusesque
+Ebrahim (Ebby) Badawi
+
+
+******************/
+
+$(document).ready(function () {
+  // This code will run when the document is ready
+});
