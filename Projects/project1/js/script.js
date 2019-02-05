6cad3003103b3b3488f2816d3c8d39511c1b5f36
Ebby <ebi.badavi@yahoo.com>
2019-02-05 12:43:59 -0500

    The Fundation
    ****HTML****
    + minor organization
    
    ****CSS****
    + Basic styles for things
    + Van Gogh's room added as the game environment
    + Bin placed in the scene
    
    ****JS****
    + Hence, ear added instead of trash ... now everything makes sense
    + Ear is draggable an bin is droppable
    + ear is added to the scene throgh addEar() function, because ears gonna be added one by one into the scene in random places

--- Projects/project1/js/script.js
+++ Projects/project1/js/script.js
@@ -8,7 +8,6 @@ Ebrahim (Ebby) Badawi
 ******************/
 
 let $bin;
-let $trash;
 
 $(document).ready(function () {
   //  Get the bin element from the page
@@ -16,7 +15,37 @@ $(document).ready(function () {
   //  make it droppable
   $bin.droppable({
     //  when drop is compeleted, call trashDropped function
-    drop: trashDropped
+    drop: earDropped
   });
+//  add an ear inside the room
+addEar();
 
 });
+
+//  Called when a draggable element (an ear) is dragged over the droppable element
+function earDropped(event,ui){
+   //  remove the ear (throw it in to the garbage)
+   ui.draggable.remove();
+   //  make the bin look full => replace it with another picture
+   $(this).attr('src','assets/images/trash_full.png');
+   //  add another ear into the space
+   addEar();
+}
+
+//  called whenever an ear is needed in the room
+function addEar(){
+  //  Awesomely create an imgage of ear and store it in a variable.
+  let $ear = $('<img class="ear" src="assets/images/ear.png" >');
+  // $ear.attr('title','ear');
+
+  //  Drop the ear "somewhere" in the room
+  $('#room').append($ear);
+
+  //  and here we calculate where should be the "somewhere" that I mentioned earlier
+  //  by using .offset() in order to give it a random position on the screen.
+  $ear.offset({
+    top: Math.random() * ($(window).height() - $ear.height()),
+    left: Math.random() * ($(window).width() - $ear.width())
+  });
+  $('.ear').draggable();
+}
