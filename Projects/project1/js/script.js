3ba7351192a7fba4caa97282143697343b1e4181
Ebby <ebi.badavi@yahoo.com>
2019-02-05 20:13:44 -0500

    emptyBin() added
    **** JS ****
    + everything that happened after the bin got full have moved to a separate function called theBinIsFull()
    
    + after the alert, player can empty the bin by clicking on it
    
    + emptyBin() function added, a function in which player is asked whether or not s/he wants to empty the bin

--- Projects/project1/js/script.js
+++ Projects/project1/js/script.js
@@ -14,6 +14,17 @@ let $bin;
 //  a variable to keep record of hoe many ears have been thrown into the trash
 let earCounter = 0;
 
+let thinking = [
+  "What the fuck?",
+  "You wanna THINK about it ?!",
+  "DUDE COME ON !!!",
+  "as if there is another option",
+  "Aweseme, you're a genius! knock on wood",
+  "relax dude, you can think, you have alllll day to think about this MINDBLOWING question",
+  "are you done ?!",
+  "come on, he has a bunch more"
+]
+
 $(document).ready(function() {
   //  Get the bin element from the page
   $bin = $('#bin');
@@ -26,6 +37,8 @@ $(document).ready(function() {
   addEar();
 
 });
+
+//+++++++++++++++++++++>>>>> earDropped() <<<<<+++++++++++++++++++++++++++
 
 //  Called when a draggable element (an ear) is dragged over the droppable element
 function earDropped(event, ui) {
@@ -40,38 +53,12 @@ function earDropped(event, ui) {
     //  add another ear
     addEar();
   } else {
-    //  create a div which will turn to the dialog box
-    let $emptyAlert = $('<div></div>');
-    //  give it an awesome title
-    $emptyAlert.attr('title', 'Alert');
-    //  add a super meaningful message into it
-    $emptyAlert.append("<p>The room is clean now and the bin is all ears, wainting for you to empty it</p>");
-    //  and add it to the page (make it appear)
-    $('body').append($emptyAlert);
-
-    //  now we make it look more dialog boxish
-    $emptyAlert.dialog({
-      //  add an okay button to it so we make sure that player agrees with us about
-      //  the fact that the bin should be emptied
-      buttons: {
-        "Okay?": function() {
-          //  close the alert box when player understood what to do
-          $(this).dialog('close');
-        }
-      },
-      //  when the alert box closes, do this stuff
-      close: function() {
-
-
-      },
-      //  it will be
-      // contained within the body tag, and can't be dragged out of it.
-      containment: 'body'
-    });
-
+    theBinIsFull();
   }
 
 }
+
+//+++++++++++++++++++++>>>>> addEar() <<<<<+++++++++++++++++++++++++++
 
 //  called whenever an ear is needed in the room
 function addEar() {
@@ -88,4 +75,83 @@ function addEar() {
     left: Math.random() * ($(window).width() - $ear.width())
   });
   $('.ear').draggable();
+}
+
+//+++++++++++++++++++++>>>>> theBinIsFull() <<<<<+++++++++++++++++++++++++++
+
+//  called whenever all ears have been thrown to the garbage
+function theBinIsFull() {
+  //  create a div which will turn to the dialog box
+  let $alert = $('<div></div>');
+  //  give it an awesome title
+  $alert.attr('title', 'Alert');
+  //  add a super meaningful message into it
+  $alert.append("<p>The room is clean now and the bin is all ears, wainting for you to empty it</p>");
+  //  and add it to the page (make it appear)
+  $('body').append($alert);
+
+  //  now we make it look more dialog boxish
+  $alert.dialog({
+    //  add an okay button to it so we make sure that player agrees with us about
+    //  the fact that the bin should be emptied
+    buttons: {
+      "Okay?": function() {
+        //  close the alert box when player understood what to do
+        $(this).dialog('close');
+      }
+    },
+    //  when the alert box closes, do this stuff
+    close: function() {
+      //  if the bin is clicked, call the emptyBin() function
+      $bin.on('click', emptyBin);
+    },
+    //  it will be
+    // contained within the body tag, and can't be dragged out of it.
+    containment: 'body'
+  });
+}
+
+//+++++++++++++++++++++>>>>> emptyBin() <<<<<+++++++++++++++++++++++++++
+
+//  called when player clicks on the full bin (when all ears all in it)
+function emptyBin() {
+  //  again, this div will turn to a dialog box
+  let $emptyAlert = $('<div></div>');
+  //  name the dialog box awesomely
+  $emptyAlert.attr('title', 'Are You SURE ??!');
+  //  add a super meaningful question into it
+  $emptyAlert.append("<p>Are you sure you want to get rid of Van Gogh's ears ?!</p>");
+  //  then, add it to the page
+  $('body').append($emptyAlert);
+
+  //  make it look good
+  $emptyAlert.dialog({
+    //  add an okay button to it so we make sure that player agrees with us about
+
+    buttons: {
+      "Yup!": function() {
+        //  close the alert box
+        $(this).dialog('close');
+      },
+      "let me think again!": function() {
+        //  convince player that there is no other way, and the bin have to be emptied
+        let thinkingResponse = thinking[Math.floor(Math.random() * thinking.length)];
+        $emptyAlert.append("<p>"+ thinkingResponse +"</p>");
+      }
+    },
+    //  when the alert box closes, empty the bin, and call changePhase() function
+    close: function() {
+      $bin.attr('src', 'assets/images/trash_empty.png');
+      changePhase();
+    },
+    //  it will be
+    // contained within the body tag, and can't be dragged out of it.
+    containment: 'body'
+  });
+}
+
+//+++++++++++++++++++++>>>>> changePhase() <<<<<+++++++++++++++++++++++++++
+
+function changePhase() {
+
 }
