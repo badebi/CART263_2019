b01c9713a16a55aa840378b64bd1d2570618109f
Ebby <ebi.badavi@yahoo.com>
2019-02-28 01:29:38 -0500

    finally it works

--- Assignments/assignment4/js/script.js
+++ Assignments/assignment4/js/script.js
@@ -12,30 +12,69 @@ let $fly;
 
 let buzz = new Audio('assets/sounds/buzz.mp3');
 let crunch = new Audio('assets/sounds/crunch.wav');
+let swallow = new Audio('assets/sounds/swallow.wav');
+let burp = new Audio('assets/sounds/burp.wav');
+let choke = new Audio('assets/sounds/choke.wav');
+let scream = new Audio('assets/sounds/scream.wav');
+
 
 // A $( document ).ready() block.
 $(document).ready(function() {
-  console.log("ready!");
-  $mouth = $('#mouth');
-  $fly = $('#fly');
 
-  $fly.draggable();
+  $mouth = $('#mouth');
   $mouth.droppable({
+    //   out: function(event, ui){
+    //     if (ui.draggable.attr('id') === 'food')){
+    //       ui.draggable("option", "revert", false);
+    //     };
+    //   },
+    //   over: function(event, ui){
+    //     if (ui.draggable.attr('id') === 'food'){
+    //       ui.draggable("option", "revert", true);
+    //   };
+    // },
     drop: flyDropped
   });
 
+  $fly = $('#fly');
+  $fly.draggable();
+
+  buzz.loop = true;
+
+  $food = $('#food');
+  $food.draggable({
+    revert: "valid",
+    // start: function(event,ui){
+    //   scream.loop = true;
+    //   scream.play();
+    // },
+    // stop: function(event,ui) {
+    //   scream.pause();
+    // }
+  });
+
   $fly.on('mousedown', function() {
+
     buzz.play();
-    $mouth.on('mouseover', function(){
-      $mouth.attr('src', 'assets/images/cartoonMouthChewOpen1.png');
-    });
-    $mouth.on('mouseout', function(){
-      $mouth.attr('src', 'assets/images/cartoonMouthChewHungry.png');
-    });
+    $mouth.attr('src', 'assets/images/cartoonMouthChewOpen1.png');
+    // $mouth.on('mouseout', function(){
+    //   $mouth.attr('src', 'assets/images/cartoonMouthChewHungry.png');
+    // });
   });
 
   $fly.on('mouseup', function() {
     buzz.pause();
+    $mouth.attr('src', 'assets/images/cartoonMouthSmile.png');
+  });
+
+  $food.on('mousedown', function() {
+    $mouth.attr('src', 'assets/images/cartoonMouthChewYuck.png');
+    scream.loop = true;
+    scream.play();
+  });
+
+  $food.on('mouseup', function() {
+    scream.pause();
     $mouth.attr('src', 'assets/images/cartoonMouthSmile.png');
   });
 
@@ -58,25 +97,31 @@ $(document).ready(function() {
 
 });
 
-console.log('in here');
-
 function flyDropped(event, ui) {
   console.log("Dropped!");
-
   // $('#fly').remove();
   // $fly.remove();
-  ui.draggable.remove();
-  crunch.play();
-  let chewInterval = setInterval(chew, 300);
-  setTimeout(function() {
-    clearInterval(chewInterval);
-    flyEaten();
-  }, 2000);
+  if (ui.draggable.attr('id') === "fly") {
+    ui.draggable.remove();
+    crunch.play();
+    $mouth.attr('src', 'assets/images/cartoonMouthChewClosed.png');
+    let chewInterval = setInterval(chew, 300);
+    setTimeout(function() {
+      clearInterval(chewInterval);
+      $mouth.attr('src', 'assets/images/cartoonMouthSmile.png');
+      flyEaten();
+    }, 5000);
+  } else {
+    choke.play();
+  };
 }
 
 function flyEaten() {
   console.log('fly has been eaten');
-
+  swallow.play();
+  swallow.addEventListener("ended", function() {
+    burp.play();
+  });
   $mouth.on('mouseleave', function() {
     $mouth.attr('src', 'assets/images/cartoonMouthSmile.png');
   });
