fdb5bb7e155ef232b16a31a71ca5ffc1ed39afd6
Ebby <ebi.badavi@yahoo.com>
2019-03-12 10:03:28 -0400

    ACTIVITY DONE

---
+++ ClassActivity/Week9/Condiments/js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+$(document).ready(function () {
+  console.log("ready");
+  $.getJSON('/data/data.json', dataLoaded);
+});
+
+function dataLoaded(data) {
+  console.log(data);
+
+  let condiment = getRandomElement(data.condiments);
+  console.log("condiment => " + condiment);
+
+  let verb = "is";
+  if (condiment.charAt(condiment.length - 1) === "s") {
+    verb = "are";
+  }
+  console.log("verb => " + verb);
+
+  let cat = getRandomElement(data.cats);
+  console.log("cat => " + cat);
+
+  let room = getRandomElement(data.rooms);
+  console.log("room => " + room);
+
+  let description = `${condiment} ${verb} like a ${cat} in a ${room}.`
+  $('body').append(`<div>${description}</div>`);
+}
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
