eb0ef8018d0c8d2d1d26f340b0d0718af0062454
Ebby <ebi.badavi@yahoo.com>
2019-03-12 23:59:32 -0400

    Intoduction to the conversation
    ++ Added intro dialog
    ++ Added annyang commands
    ++ Added bringInTheCard() function to bring in the card when its needed
    ++ Added tekeOutTheCard() to use it after
    ++ Added startInterrogation() function to begin and handle the start of conversation

--- Projects/project2/js/script.js
+++ Projects/project2/js/script.js
@@ -9,30 +9,93 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let magicNumber;
+let intro = [
+  "When ever you're ready, just tell me I'm ready",
+  "don't you want to know why should you be ready?",
+  "you're not the one who is asking the questions here! I ask questions. now tell me your name",
+  "just answer the question, what is your name, just the name!",
+  "why did you click on the page? we start from the top then! what is your name?"
+]
+let introLineIndex = 0;
 
+let clickedOnce = false;
+
+let voiceParameters = {
+  pitch: 1,
+  rate: 0.75,
+  volume: 0.5,
+}
+ let command, command2;
 
 $(document).ready(setup);
 
 function setup() {
-  let magicNumber = -1.35 * ($(".handAndCard").innerHeight());
+  magicNumber = -1.35 * ($(".handAndCard").innerHeight());
   // $(".handAndCard").innerHeight(magicNumber);
-  $(".handAndCard").animate({"top":  magicNumber});
-  $("body").click(function () {
-    $(".handAndCard").toggle().animate({"top":  0},2000,"swing");
-    $("#hand").animate({"top":  magicNumber},2000, "swing");
+  $(".handAndCard").animate({
+    "top": magicNumber
   });
 
-
+  $("body").on("click", function() {
+    if (!clickedOnce) {
+      responsiveVoice.speak(intro[0], 'UK English Male', voiceParameters);
+      introLineIndex ++;
+      clickedOnce = !clickedOnce;
+    } else {
+      responsiveVoice.speak(intro[intro.length - 1], 'UK English Male', voiceParameters);
+    }
+  });
 
   if (annyang) {
-    var command = {
-
+     command = {
+      "I'm ready": startInterrogation,
+      "I am ready": startInterrogation,
+      "why": startInterrogation,
     }
+
+    command2 = {
+      "*tag": name
+    }
+
     annyang.addCommands(command);
     annyang.start();
   }
 }
 
+function startInterrogation() {
+  if (introLineIndex === intro.length - 2) {
+    // Remove intro commands
+    responsiveVoice.speak(intro[introLineIndex], 'UK English Male', voiceParameters);
+    annyang.removeCommands();
+    annyang.addCommands(command2);
+    return;
+  }
+  responsiveVoice.speak(intro[introLineIndex], 'UK English Male', voiceParameters);
+  introLineIndex ++;
+  if (introLineIndex === intro.length - 2 && annyang.isListening()) {
+    startInterrogation();
+    console.log("tell me your name");
+  }
+}
+
+function name(name) {
+  console.log("name");
+  responsiveVoice.speak(`${name}?`, 'UK English Male', voiceParameters);
+}
+
+function bringInTheCard() {
+  $(".handAndCard").toggle().animate({
+    "top": 0
+  }, 2000, "swing");
+  $("#hand").animate({
+    "top": magicNumber
+  }, 2000, "swing");
+}
+
+function takeOutTheCard() {
+
+}
 
 /*
  * Shuffles array in place. In case we need it
