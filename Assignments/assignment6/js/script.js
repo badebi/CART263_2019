e0133043daff65febca6102b88ee704efcd10d7a
badebi <ebi.badavi@yahoo.com>
2019-03-21 13:29:48 -0400

    Almost DONE
    + Added array of vowels to check the first character of names with it in order to know what article to use

--- Assignments/assignment6/js/script.js
+++ Assignments/assignment6/js/script.js
@@ -10,6 +10,8 @@ author, and this description to match your project!
 
 ******************/
 
+let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
 $(document).ready(function() {
   console.log("ready");
   $.getJSON('/data/data.json', dataLoaded);
@@ -17,8 +19,12 @@ $(document).ready(function() {
 
 function dataLoaded(data) {
   console.log(data);
-  giveMeADescription(data)
+  giveMeADescription(data);
+  $("body").click(function() {
+    location.reload();
+  });
 }
+
 
 function getRandomElement(array) {
   return array[Math.floor(Math.random() * array.length)];
@@ -37,9 +43,19 @@ function giveMeADescription(data) {
   let cat = getRandomElement(data.cats);
   console.log("cat => " + cat);
 
+  let catArticle = "a";
+  if (vowels.includes(cat.charAt(0))) {
+    catArticle = "an";
+  }
+
   let room = getRandomElement(data.rooms);
   console.log("room => " + room);
 
-  let description = `${condiment} ${verb} like a ${cat} in a ${room}.`
+  let roomArticle = "a";
+  if (vowels.includes(room.charAt(0))) {
+    roomArticle = "an";
+  }
+
+  let description = `${condiment} ${verb} like ${catArticle} ${cat} in ${roomArticle} ${room}.`
   $('body').append(`<div id= "des">${description}</div>`);
 }
