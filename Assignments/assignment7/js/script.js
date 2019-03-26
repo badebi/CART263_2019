c082dbe46894af443f7a5786ef2e613ed893413f
Ebby <ebi.badavi@yahoo.com>
2019-03-26 12:36:07 -0400

    effects Added

--- Assignments/assignment7/js/script.js
+++ Assignments/assignment7/js/script.js
@@ -11,7 +11,9 @@ author, and this description to match your project!
 ******************/
 // kick= x ,snare= o ,hihat= *, dog = d
 
-let pattern = ['x','*','x','xo*','*','*o','x','od'];
+const NOTE_LENGTH = 500;
+
+let pattern = ['x', '*', 'x', 'xo*', '*', '*o', 'x', 'od'];
 let patternIndex = 0;
 
 let frequencies = [110.00, 123.47, 138.59, 146.83, 164.81, 185.00, 207.65];
@@ -21,7 +23,16 @@ let kick;
 let snare;
 let hihat;
 let dog;
+
+let delay;
+let dubDelay;
+let tremolo;
+let lowPassFilter;
+
 let isStarted = false;
+
+let multiplier;
+let prvMultiplier;
 
 // preload()
 //
@@ -43,10 +54,38 @@ function setup() {
       frequency: 440
     }
   });
+
+  delay = new Pizzicato.Effects.Delay({
+    feedback: 0.6,
+    time: 0.4,
+    mix: 0.5
+  });
+
+  dubDelay = new Pizzicato.Effects.DubDelay({
+    feedback: 0.57,
+    time: 0.42,
+    mix: 0.5,
+    cutoff: 700
+  });
+
+  tremolo = new Pizzicato.Effects.Tremolo({
+    speed: 7,
+    depth: 0.8,
+    mix: 0.8
+  });
+
+  lowPassFilter = new Pizzicato.Effects.LowPassFilter({
+    frequency: 400,
+    peak: 10
+  });
+
   kick = new Pizzicato.Sound('assets/sounds/kick.wav');
   snare = new Pizzicato.Sound('assets/sounds/snare.wav');
   hihat = new Pizzicato.Sound('assets/sounds/hihat.wav');
   dog = new Pizzicato.Sound('assets/sounds/bark.wav');
+
+  kick.addEffect(dubDelay);
+  synth.addEffect(lowPassFilter);
 }
 
 
@@ -61,17 +100,48 @@ function draw() {
 function mousePressed() {
   // NOTE: to fix the variable intervals
   if (!isStarted) {
-      setInterval(playNote, 500);
-      setInterval(playDrum, 250);
-      isStarted = true;
+    //setInterval(playNote, NOTE_LENGTH);
+    setTimeout(playNote, NOTE_LENGTH);
+    setInterval(playDrum, 250);
+    isStarted = true;
   }
 
 }
 
+function mouseMoved() {
+  console.log(lowPassFilter.frequency);
+  let newFreq = map(mouseX, 0, windowWidth, 70, 420);
+  lowPassFilter.frequency = newFreq;
+  // synth.removeEffect(lowPassFilter);
+  // synth.addEffect(lowPassFilter);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+
+  }
+  if (keyCode === DOWN_ARROW) {
+
+  }
+}
+
 function playNote() {
-  let frequency = getRandomFrom(frequencies);
-  synth.frequency = frequency;
-  synth.play();
+  multiplier = floor(random() * 7);
+
+  let ran = random();
+  if (ran > 0.42) {
+    let frequency = getRandomFrom(frequencies);
+    synth.frequency = frequency;
+    synth.attack = prvMultiplier / 3.3;
+    synth.release = prvMultiplier / 3.3;
+    synth.play();
+  } else {
+    synth.stop();
+    multiplier = 1;
+  }
+  prvMultiplier = multiplier;
+  setTimeout(playNote, multiplier * NOTE_LENGTH);
+
 }
 
 function playDrum() {
@@ -80,16 +150,16 @@ function playDrum() {
   // }
   let symbols = pattern[patternIndex];
 
-  if(symbols.indexOf('x') != -1) {
+  if (symbols.indexOf('x') != -1) {
     kick.play();
   }
-  if(symbols.indexOf('o') != -1) {
+  if (symbols.indexOf('o') != -1) {
     snare.play();
   }
-  if(symbols.indexOf('*') != -1) {
+  if (symbols.indexOf('*') != -1) {
     hihat.play();
   }
-  if(symbols.indexOf('d') != -1) {
+  if (symbols.indexOf('d') != -1) {
     dog.play();
   }
   // patternIndex ++;
